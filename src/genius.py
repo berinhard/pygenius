57dafde8116fa676bf8d5ac4ecb721ed1739d341
Bernardo Fontes <bernardoxhc@gmail.com>
2010-10-12 20:06:04 -0300

    Garante o fluxo de turnos

--- src/genius.py
+++ src/genius.py
@@ -2,10 +2,11 @@
 import pygame
 import time
 import random
+import sys
 
 from genius_classes import GeniusRect
 
-global screen, genius_rect, images
+global screen, genius_rect, images, player_time
 
 size = (660, 660)
 
@@ -37,12 +38,24 @@ def get_random_color():
     color_list.remove('genius')
     return random.choice(color_list)
 
+def mouse_click(event):
+    left = 1
+    return event.type == pygame.MOUSEBUTTONDOWN and event.button == left
+
 def main_loop():
     color_list = []
+    player_time = False
 
     while True:
-        color_list.append(get_random_color())
-        blink_list(color_list)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+            elif player_time and mouse_click(event):
+                player_time = False
+        if not player_time:
+            color_list.append(get_random_color())
+            blink_list(color_list)
+            player_time = True
 
 if __name__ == '__main__':
     main_loop()
